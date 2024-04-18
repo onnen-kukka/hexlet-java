438ac432dc23b59e986a3f2329001888ac3d7f5d
Eugenia Zolotova <zolotovarabota@gmail.com>
2024-04-19 02:47:33 +0300

    Add Gradle

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,8 +1,11 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+        var reversedWord = StringUtils.reverse("Hexlet");
+        System.out.println(reversedWord);
     }
 }
 
